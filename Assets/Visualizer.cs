58d6fd128648e466cf87d7149426a889bf6fc9cf
Pritish Choudhary <pritish.choudhary@yahoo.in>
2017-03-15 15:38:44 +1000

    no message

--- Assets/Visualizer.cs
+++ Assets/Visualizer.cs
@@ -28,21 +28,17 @@ public class Visualizer : MonoBehaviour {
     private Transform[] visualList;
     private float[] visualScale;
     public int amnVisual = 256;
-    // public GameObject ps;
-    public GameObject[] particlesystemsgameobjects;// = new GameObject[64];
-    private ParticleSystem[] particlesystems;// = new ParticleSystem[64];
+    public GameObject[] psGameObject;
+    private ParticleSystem[] particlesystems;
     public GameObject flameObject;
 
     // Use this for initialization
     void Start () {
-        particlesystems = new ParticleSystem[256];
-        particlesystemsgameobjects = new GameObject[256];
-        //for (int i = 0; i < particlesystemsgameobjects.Length; i++)
-        //{
-        //    particlesystems[i] = particlesystemsgameobjects[i].GetComponent<ParticleSystem>();
-        //    particlesystemsgameobjects[i].transform.position = new Vector3(i, 0, 0);
-        //}
 
+        //Initalize
+        particlesystems = new ParticleSystem[256];
+        psGameObject = new GameObject[256];
+      
         source = GetComponent<AudioSource>();
         sampleRate = AudioSettings.outputSampleRate;
 
@@ -105,9 +101,7 @@ public class Visualizer : MonoBehaviour {
         }
 
         pitchValue = frequency * (sampleRate / 2) / SAMPLE_SIZE;
-        //ParticleSystem p = ps.GetComponent<ParticleSystem>();
-        //p.Emit((int)(spectrum[1]*1000.0f));
-
+     
     }
 
     private void SpawnLine()
@@ -117,91 +111,50 @@ public class Visualizer : MonoBehaviour {
 
         for (int i = 0; i < amnVisual; i++)
         {
-            particlesystemsgameobjects[i] = (GameObject)Instantiate(flameObject, transform.position + new Vector3(i,0,0), Quaternion.Euler(-90.0f,0,0));
-            particlesystems[i] = particlesystemsgameobjects[i].GetComponent<ParticleSystem>();
+            psGameObject[i] = (GameObject)Instantiate(flameObject, transform.position + new Vector3(i,0,0), Quaternion.Euler(-90.0f,0,0));
+            particlesystems[i] = psGameObject[i].GetComponent<ParticleSystem>();
         }
-           
-
-        //for (int i = 0; i < amnVisual; i++)
-        //{
-
-        //    //particlesystems[i].Emit((int)(spectrum[i] * 1000.0f));
-        //    //particlesystems[i].Emit(100);
-        //    GameObject go = GameObject.AddComponent(ParticleSystem);
-        //    particlesystems[i].Emit((int)(spectrum[i] * 1000.0f));
-        //    visualList[i] = particlesystems[i].transform;
-        //    visualList[i].position = Vector3.right * i;
-        //}
+        
     }
 
-    private void SpawnCircle()
-    {
-        visualScale = new float[amnVisual];
-        visualList = new Transform[amnVisual];
+    //private void SpawnCircle()
+    //{
+    //    visualScale = new float[amnVisual];
+    //    visualList = new Transform[amnVisual];
 
-        Vector3 centre = Vector3.zero;
-        float radius = 10.0f;
+    //    Vector3 centre = Vector3.zero;
+    //    float radius = 10.0f;
 
-        for (int i = 0; i < amnVisual; i++)
-        {
-            float angle = i*1.0f / amnVisual;
-            angle *= Mathf.PI * 2;
+    //    for (int i = 0; i < amnVisual; i++)
+    //    {
+    //        float angle = i*1.0f / amnVisual;
+    //        angle *= Mathf.PI * 2;
 
-            float x = centre.x + Mathf.Cos(angle) * radius;
-            float y = centre.y + Mathf.Sin(angle) * radius;
+    //        float x = centre.x + Mathf.Cos(angle) * radius;
+    //        float y = centre.y + Mathf.Sin(angle) * radius;
 
-            Vector3 pos = new Vector3(x,y,0);
-            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            go.transform.position = pos;
-            go.transform.rotation = Quaternion.LookRotation(Vector3.forward, pos);
-            visualList[i] = go.transform;
-        }
+    //        Vector3 pos = new Vector3(x,y,0);
+    //        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //        go.transform.position = pos;
+    //        go.transform.rotation = Quaternion.LookRotation(Vector3.forward, pos);
+    //        visualList[i] = go.transform;
+    //    }
 
 
-    }
+    //}
 
     private void UpdateVisual()
     {
-        int visualIndex = 0;
-        int spectrumIndex = 0;
-        int avgSize = (int)(SAMPLE_SIZE * keepPercentage) / amnVisual;
+       
 
         for (int i = 0; i < amnVisual; i++)
-        {
-            // particlesystems[i].Emit(100);
-            //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);     
-            
+        {                        
             particlesystems[i].Emit((int)(spectrum[i] * 1000.0f)+10);
             visualList[i] = particlesystems[i].transform;
             visualList[i].position = Vector3.right * i;
             ParticleSystem.MainModule mm = particlesystems[i].main;
             mm.startSpeed = spectrum[i] * 300.0f +5;
-        }
-
-        //while (visualIndex < amnVisual)
-        //{
-        //    int j = 0;
-        //    float sum = 0;
-        //    while (j < avgSize)
-        //    {
-        //        sum += spectrum[spectrumIndex];
-        //        spectrumIndex++;
-        //        j++;
-        //    }
-
-        //    float scaleY = sum / avgSize * visualModifier;
-        //    visualScale[visualIndex] -= Time.deltaTime * smoothSpeed;
-
-        //    if (visualScale[visualIndex] < scaleY)
-        //        visualScale[visualIndex] = scaleY;
-
-        //    if (visualScale[visualIndex] > maxVisualModifier)
-        //        visualScale[visualIndex] = maxVisualModifier;
-
-        //    visualList[visualIndex].localScale = Vector3.one + Vector3.up * visualScale[visualIndex];
-        //    visualIndex++;
-
-        //}
+        }       
     }
 
     private void UpdateBackground()
